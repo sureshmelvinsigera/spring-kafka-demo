27995fb93eead7a48b3eedc18d5af915ec6b251c
sureshmelvinsigera <sureshsigera@gmail.com>
2023-09-19 17:13:38 -0400

    twitter kafka microservice setup

---
+++ twitter-to-kafka/src/main/java/com/twitter/kafka/service/TwitterKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.twitter.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterKafkaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterKafkaServiceApplication.class, args);
+    }
+}
